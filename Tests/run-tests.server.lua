5f42c8a6373a041846cbc399cd85a6f4136bc3c6
Naeius <naeius514@gmail.com>
2026-09-09 22:25:28 -0400

    Revisions.

---
+++ Tests/run-tests.server.lua
@@ -0,0 +1,12 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Jest = require(ReplicatedStorage.Packages.Jest)
+
+local status, result = Jest.runCLI(ServerScriptService.Tests, { verbose = true }, { ServerScriptService.Tests })
+	:awaitStatus()
+
+if status == "Resolved" and result.results.numFailedTestSuites == 0 and result.results.numFailedTests == 0 then
+	return "All tests passed"
+end
+
+error("Tests failed: " .. tostring(status))
